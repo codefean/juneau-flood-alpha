bccfe3d2edb7f807e11329e37492141dc2391ce9
Sean Fagan <seanfagan@Seans-MacBook-Pro.local>
2025-03-21 12:12:44 -0800

    logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,10 @@ import FloodWarn from './FloodWarn';
 const Header = () => {
   return (
     <header className="header">
-      <div>
+      <div className="header-image">
+        <img src="/ACASC2.png" alt="Alaska Climate Adaptation Science Center Logo" />
+      </div>
+      <div className="header-title">
         <h1>Juneau Glacial Flood Dashboard</h1>
       </div>
       <FloodWarn />
