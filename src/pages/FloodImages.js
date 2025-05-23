4a8edb05a6c89b3520c380aa6d394703d2a2ff26
codefean <emailfean001@gmail.com>
2025-05-23 13:35:11 +0200

    update

--- src/pages/FloodImages.js
+++ src/pages/FloodImages.js
@@ -8,18 +8,19 @@ import "slick-carousel/slick/slick-theme.css";
 
 const FloodImage = () => {
   const images = [
+
     {
-      src: "https://www.climate.gov/sites/default/files/2024-08/SuicideBasinOutburst2024.gif",
-      title: "2024",
+      src: "https://www.weather.gov/images/ajk/suicideBasin/archive/2022/SuicideBasinLoop_Raw_2022_Compressed.gif",
+      title: "2022",
     },
     {
       src: "https://www.weather.gov/images/ajk/suicideBasin/archive/2023/SuicideBasinLoop_Raw_2023_Compressed.gif",
       title: "2023",
     },
     {
-      src: "https://www.weather.gov/images/ajk/suicideBasin/archive/2022/SuicideBasinLoop_Raw_2022_Compressed.gif",
-      title: "2022",
-    },
+      src: "https://www.climate.gov/sites/default/files/2024-08/SuicideBasinOutburst2024.gif",
+      title: "2024",
+    }
   ];
 
   const [currentSlide, setCurrentSlide] = useState(0);
