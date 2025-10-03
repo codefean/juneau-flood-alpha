643222ca61f46b6825c6f3590d6efaa60cfb6374
codefean <emailfean001@gmail.com>
2025-10-03 12:56:08 -0700

    feedback

--- src/pages/feedback.js
+++ src/pages/feedback.js
@@ -6,7 +6,7 @@ const Feedback = () => {
   return (
     <div className="feedback-container">
       <iframe
-        src="https://docs.google.com/forms/d/e/1FAIpQLScmhx9zLxDH_LQgWhzd8P0XxbL1MvAgCb6JIwk1rPXhF6HzMw/viewform?usp=header"
+        src="https://docs.google.com/forms/d/e/1FAIpQLScpAxn2kehZVgTVavv0DLuR0tRNAMwqEQnGI-NzFGt831lS1A/viewform?usp=header"
         width="100%"
         height="720px"
         allowFullScreen
