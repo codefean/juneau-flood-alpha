a3e9910b1eae6c80a8c13b3b7741faae3985351d
codefean <emailfean001@gmail.com>
2025-10-15 13:16:53 -0700

    no scroll

--- src/pages/FloodLevels.js
+++ src/pages/FloodLevels.js
@@ -369,6 +369,13 @@ popupRef.current
     return () => clearInterval(interval);
   }, []);
 
+    useEffect(() => {
+    document.body.style.overflow = 'hidden';
+    return () => {
+      document.body.style.overflow = 'auto';
+    };
+  }, []);
+
   return (
     <div className="main-content floodlevels-page">
 
